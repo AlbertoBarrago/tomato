318a4d32fece3b0db8f45d49e83f53341c203c5e
Alberto Barrago <albertobarrago@gmail.com>
2024-10-18 22:43:50 +0200

    Tomato boilerplate

--- main.py
+++ main.py
@@ -1,42 +1,69 @@
-from logging.config import stopListening
 from tkinter import Tk, Canvas, PhotoImage, Label, Button
 import math
-# ---------------------------- CONSTANTS ------------------------------- #
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 5 * 60
+SHORT_BREAK_MIN = 5 * 60
+LONG_BREAK_MIN = 20 * 60
+TITLE_PLACEHOLDER = "Start Timer"
 
-window = Tk()
-
+reps = 0
 STOP_COUNT = False
+
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    window.config(stopListening())
+    global reps, STOP_COUNT
+    STOP_COUNT = True
+    reps = 0
     canvas.itemconfig(timer_text, text="00:00")
-    pass
+    title_label.config(text=TITLE_PLACEHOLDER, fg=PINK)
+    check_mark.config(text="")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(1 * 60)
+    global reps, STOP_COUNT
+    STOP_COUNT = False
+    reps += 1
+    if reps % 8 == 0:
+        title_label.config(text="Long Break", fg=RED)
+        count_down(LONG_BREAK_MIN)
+    elif reps % 2 == 0:
+        title_label.config(text="Short Break", fg=PINK)
+        count_down(SHORT_BREAK_MIN)
+    else:
+        title_label.config(text="Work Time", fg=GREEN)
+        count_down(WORK_MIN)
 
-# ---------------------------- UI SETUP ------------------------------- #
 
-def count_down(count):
-    count_min = math.floor(count / 60)
-    count_sec = count % 60
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(seconds):
+    global STOP_COUNT
+    if STOP_COUNT:
+        return
+
+    count_min = math.floor(seconds / 60)
+    count_sec = seconds % 60
     count_min, count_sec = handle_time(count_min, count_sec)
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
-    if count > 0:
-        window.after(1000, count_down, count - 1)
+
+    if seconds > 0:
+        window.after(1000, count_down, seconds - 1)
+    else:
+        start_timer()
+        marks = ""
+        for _ in range(math.floor(reps / 2)):
+            marks += "✔"
+        check_mark.config(text=marks)
 
 
+# ---------------------------- HELPER FUNCTION ------------------------------- #
 def handle_time(count_min, count_sec):
     """
     Handling sec and minutes to print 00:00 in case of < 10 or 0
@@ -51,30 +78,30 @@ def handle_time(count_min, count_sec):
     return count_min, count_sec
 
 
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-window.grid_rowconfigure(0, weight=1)
 
-title_label = Label(window, text="Timer", font=(FONT_NAME, 30, "bold"), background=YELLOW, foreground=PINK)
-title_label.grid(column=1, row=0, columnspan=2, padx=5, pady=2)
+title_label = Label(window, text=TITLE_PLACEHOLDER, font=(FONT_NAME, 30, "bold"), background=YELLOW, foreground=PINK)
+title_label.grid(column=1, row=0)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, borderwidth=0, highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(column=1, row=2, columnspan=2, pady=20)
+canvas.grid(column=1, row=1)
 
-button_start = Button(window, text="Start", command=lambda: start_timer(), highlightthickness=0)
+button_start = Button(window, text="Start", command=start_timer, highlightthickness=0)
 button_start.config(highlightbackground=YELLOW, highlightcolor=YELLOW)
-button_start.grid(column=0, row=5, columnspan=2)
+button_start.grid(column=0, row=2)
 
-mark = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30, "bold"))
-mark.grid(column=1, row=5, columnspan=2)
+button_reset = Button(window, text="Reset", command=reset_timer, highlightthickness=0)
+button_reset.config(highlightbackground=YELLOW, highlightcolor=YELLOW)
+button_reset.grid(column=2, row=2)
 
-button_end = Button(window, text="Reset", command=lambda: reset_timer(), highlightthickness=0)
-button_end.config(highlightbackground=YELLOW, fg=RED, highlightcolor=RED)
-button_end.grid(column=2, row=5, columnspan=2)
+check_mark = Label(window, text="", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 24))
+check_mark.grid(column=1, row=3)
 
 
-window.mainloop()
+window.mainloop() #Main loop
